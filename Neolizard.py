1f2bba6ca79a0a7e327768777b0b319376afe1b6
mxvp <max.van.puyvelde@hotmail.com>
2023-07-14 00:00:53 +0200

    argparse structure finished

--- Neolizard.py
+++ Neolizard.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
+from lib.argparse_config import parse_the_args
 
 
 def main():
-
-
+    args=parse_the_args()
+    
+    
 
 
 if __name__ == '__main__':
--- lib/argparse_config.py
+++ lib/argparse_config.py
@@ -1,7 +1,29 @@
+# Have to use prefixes for subarguments, namespace conflicts will happen even if using groups
+# change args.input in main() after every operation to location necessary for next
+
+# By default, argparse will assume that any argument that starts with a hyphen (-) is an option or flag, and any argument that doesn't start with a hyphen is a positional argument. This means that as long as the arguments provided after the positional argument(s) don't start with a hyphen, argparse will interpret them as values for that positional argument.
+# nargs: multiple values are passed (added to list), "" hyphens work to capture as one value
+
+
 import argparse
 
-def parse_args():
-    # Create the argument parser
+def parse_the_args():
     parser = argparse.ArgumentParser(description='Custom pipeline for neoantigen prediction on NGS samples or TCGA MAF files.',prog="NeoLizard")
-    parser.add_argument("--input",required=True,type=str,help="Path of input file/dir")
-    subparsers=parser.add_subparsers(help="Operations to perform")
+    parser.add_argument('--input', type=str, required=True, help='Input file(s) path')
+    parser.add_argument('--qc', action='store_true', help='perform QC')
+    parser.add_argument('--m2a', action='store_true', help='Convert MAF to AVINPUT')
+
+    # Example argument group
+    cutadapt_parser = parser.add_argument_group('cutadapt', 'Optional argument: cutadapt')
+    cutadapt_parser.add_argument('--cutadapt', action='store_true', help='Enable cutadapt')
+    cutadapt_parser.add_argument('--cutadapt_trimlength', type=int, help='Subargument for cutadapt: trimlength')
+    cutadapt_parser.add_argument('--cutadapt_trimlength2', type=int, help='Subargument for cutadapt: trimlength2')
+
+    # Example nargs
+    parser.add_argument('--command', nargs='+', help='Command argument')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    return args
+
