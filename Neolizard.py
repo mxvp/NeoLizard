4835a119240dc9821f1e14ef5a2faff247d1057f
mxvp <max.van.puyvelde@hotmail.com>
2023-07-30 14:22:40 +0200

    author

--- Neolizard.py
+++ Neolizard.py
@@ -19,7 +19,7 @@ def main():
     '''
     Main function of the NeoLizard pipeline.
     Runs all requested steps.
-    author : max
+    author : mxvp
     '''
     args = parse_the_args()  # Argparse
     os.makedirs(args.output, exist_ok=True)
